2c04ec78cd3ac076f48daca9cbe985e4a04498e9
Ahmad Triadi Julianto M <ahmadtriadi265@gmail.com>
2023-11-24 23:12:48 +0800

    feat: add more tables in database

--- database/migrations/2023_11_24_102314_create_manager_table.php
+++ database/migrations/2023_11_24_102314_create_manager_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('manager', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('manager_id');
+            $table->string('username');
         });
     }
 
