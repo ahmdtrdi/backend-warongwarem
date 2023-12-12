597e2b265fc4443b20b939497ffb62445c7d8521
Regina <89380352+reregin@users.noreply.github.com>
2023-12-12 19:20:18 +0800

    feat: add and modify few files before deployment
    Co-authored-by: ahmadtriadi265@outlook.com <ahmadtriadi265@outlook.com>

---
+++ app/Http/Controllers/tablesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Table;
+use Illuminate\Http\Request;
+
+class tablesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(tablesController $tableController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(tablesController $tableController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, tablesController $tableController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(tablesController $tableController)
+    {
+        //
+    }
+}
